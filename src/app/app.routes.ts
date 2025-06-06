9ba2dfbfa2dd451dacdf2d7215bac9bd3328641b
Ken Coles <kenscoles@gmail.com>
2025-06-06 12:38:42 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+   import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes =[
+    {
+        path: '',
+        children: [
+   {
+                path: 'menu', title: 'menu', // added KSC  
+              loadComponent: () => import('./menu/menu').then(c => c.Menu)
+            },
+             {
+                path: 'comp1', title: 'comp1', // added KSC  
+              loadComponent: () => import('./comp1/comp1').then(c => c.Comp1)
+            },
+         
+        ]
+    }
+
+]
